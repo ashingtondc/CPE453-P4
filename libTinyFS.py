3017d9fd6d85bd0ba88bee5eb907251e79a4259a
remy-wolf <remywolf.r@gmail.com>
2022-03-11 03:04:07 -0800

    implemented files, directories

--- libTinyFS.py
+++ libTinyFS.py
@@ -4,13 +4,16 @@ from importlib.util import MAGIC_NUMBER
 from libDisk import BLOCKSIZE
 import libDisk as ld
 from array import *
-
+import math
+from collections import deque
 
 MAGIC_NUMBER = 0x5A
 SUPERBLOCK = 0x00
 ROOT_INODE = 0x01
 DEFAULT_DISK_SIZE = 10240
 DEFAULT_DISK_NAME = "tinyFSDisk"
+MAX_OPEN_FILES = 20
+MAX_FILENAME = 8
 
 class tinyFS:
 
@@ -46,7 +49,9 @@ class tinyFS:
         When unmounting, the Python dict is translated into the specified format
         and the block is written to the disk
         """
-        self.file_table = None
+        # self.file_table = None
+        self.file_table = [None] * MAX_OPEN_FILES # stores inode number at 
+        self.free_fds = deque(range(0, MAX_OPEN_FILES), maxlen=MAX_OPEN_FILES)
 
     def __str__(self):
         return f"\
@@ -65,6 +70,8 @@ class tinyFS:
     # metadata, etc. Must return a specified success/error code.
     def tfs_mkfs(self, filename=DEFAULT_DISK_NAME, nBytes=DEFAULT_DISK_SIZE):
         disk = ld.openDisk(filename, nBytes)
+        self.current_disk = disk
+
         """ if disk < 0: # error
             return disk # for now, return same error code """
 
@@ -79,32 +86,28 @@ class tinyFS:
         free_bytes = BLOCKSIZE - 3 # number of remaining bytes in the superblock
         max_free_blocks = free_bytes # 1 byte to track each block
         if num_free_blocks <= max_free_blocks:
-            free_block_table = [0]*num_free_blocks
+            self.free_block_table = [0]*num_free_blocks
         else:
             # Cannot support this disk size. Superblock does not have enough
             # space to track free blocks
             return -2
-        
-        
+
+        # create root directory first
+        # doing this in this order so free_block_table gets saved properly
+        root = self.create_directory(mode="new", block=ROOT_INODE, parent=ROOT_INODE)
+        data = {'block': root.inode.encode()}
+        ld.writeBlock(disk, ROOT_INODE, data)
+
         # 1st byte of superblock is magic number
         # 2nd byte of superblock is root inode block number
         # 3rd byte of superblock is number of free blocks at disk creation
         # remaining bytes are a table tracking the free blocks
 
         # 1 byte max is 255. num_free_blocks will always be less than 255. 
-        superblock_data = [MAGIC_NUMBER, ROOT_INODE, num_free_blocks] + free_block_table
+        superblock_data = [MAGIC_NUMBER, ROOT_INODE, num_free_blocks] + self.free_block_table
         superblock = {"block": array('B', superblock_data)}
         ld.writeBlock(disk, SUPERBLOCK, superblock)
 
-
-
-        # TODO: figure out inode structure
-        # hold pairs: <filename>:<block # for corresponding inode>
-        # first byte indicates number of files. initialize to 0
-        root_inode = {"block": 0}
-        ld.writeBlock(disk, ROOT_INODE, root_inode)
-
-        # anything else that needs to be initialized?
         return disk
 
     # tfs_mount(char *filename) “mounts” a TinyFS file system located within ‘filename’. 
@@ -130,31 +133,11 @@ class tinyFS:
         free_block_table = superblock_data[3:num_free_blocks + 3]
         self.free_block_table = array('B', free_block_table)
 
-        # Root inode
-        file_table = {}
-        root_inode = {}
-        ld.readBlock(disk, ROOT_INODE, root_inode)
-        root_inode_data = bytearray(root_inode['block'])
-        num_files = root_inode_data[0]
-        # Translate each filename/block pair
-        for i in range(num_files):
-            # Sliding window of 9 bytes since each pair is stored in 9 bytes
-            start = 9*i + 1
-            end = start + 9
-            pair = root_inode_data[start:end]
-            # first byte of each 9 byte sequence is the block number of the inode
-            inode_block_num = pair[0]
-            # build the name string from the remaining bytes
-            inode_name = []
-            for j in range(1, 9):
-                # look for null terminator
-                if pair[j] == 0:
-                    break
-                inode_name.append(str(pair[j]))
-            inode_name = "".join(inode_name)
-            file_table[inode_name] = inode_block_num
-
-        self.file_table = file_table
+        root_directory = self.create_directory(mode="block", block=ROOT_INODE, parent=ROOT_INODE)
+        self.root_dir_fd = self.free_fds.popleft()
+        # store open file object, file pointer in file table
+        self.file_table[self.root_dir_fd] = (root_directory, 0)
+        print(root_directory)
 
         self.mounted = True
         self.current_disk = disk
@@ -175,41 +158,49 @@ class tinyFS:
     # the internal file pointer, etc.), and returns a file descriptor (integer) that can 
     # be used to reference this file while the filesystem is mounted.
     def tfs_open(self, name):
-        if name not in self.file_table:
+        root_dir = self.file_table[self.root_dir_fd][0]
+        
+        name = name.encode('utf-8')
+        if name in root_dir.files:
+            inode = root_dir.files[name]
+            if name[-1] == "/":
+                file = self.create_directory(mode="block", block=inode)
+            else:
+                file = self.create_file(mode="block", block=inode)
+
+        else:
             # Find open block
-            block_num = self.allocate(1)
-            if block_num is None: 
+            inode = self.allocate(1)
+            if inode is None: 
                 # No free blocks.
                 return -4
-            block_num = block_num[0]
-
-            self.free_block_table[block_num - 2] = 1
+            inode = inode[0]
 
             # Update the file table
-            self.file_table[name] = block_num
+            if name[-1] == "/":
+                file = self.create_directory(mode="new", block=inode)
+            else:
+                file = self.create_file(mode="new")
+            
+            data = {'block': file.inode.encode()}
+            ld.writeBlock(self.current_disk, inode, data)
+            root_dir.add_file(name, inode)
 
-            # Create the inode
-            # TODO: write the code to build inode
-            """
-            Do we actually need to do anything here? With no idea of what's in the file
-            yet, I think we can just reserve a block and fill in the inode later
-            """
-            # first 4 bytes of inode: size of file
-            # Remaining bytes: Contiguous block numbers for data (written in the order they should be accessed)
-        inode_num = self.file_table[name]
-        # TODO: Complete this part
-        """
-        Since we can't return a real file descriptor, maybe create a class for it
-        """
+        fd = self.free_fds.popleft()
+        self.file_table[fd] = file
+        
+        return fd
     
     # Closes the file and removes dynamic resource table entry.
     def tfs_close(self, fs):
+        # remove entry from file table, add file descriptor back to queue
         pass
 
     # Writes buffer ‘buffer’ of size ‘size’, which represents an entire file’s contents, 
     # to the file described by ‘FD’. Sets the file pointer to 0 (the start of file) when done. 
     # Returns success/error codes.
     def tfs_write(self, FD, buffer, size):
+        # use file write() method
         pass
     
     # deletes a file and marks its blocks as free on disk.
@@ -224,6 +215,7 @@ class tinyFS:
 
     # Change the file pointer location to offset (absolute). Returns success/error codes.
     def tfs_seek(FD, offset):
+        # update file pointer in file_table
         pass
 
     # Allocate a certain number of free blocks.
@@ -234,6 +226,9 @@ class tinyFS:
         while i < len(self.free_block_table) and len(blocks) < num_blocks:
             if self.free_block_table[i] == 0:
                 blocks.append(i + 2)
+            i += 1
+        for block in blocks:
+            self.free_block_table[block - 2] = 1
         if len(blocks) == num_blocks:
             return blocks
         return None
@@ -241,15 +236,136 @@ class tinyFS:
     # free a data block
     def free(self, block_nums):
         for block in block_nums:
+            print(f"free {block}")
             self.free_block_table[block - 2] = 0
 
+    def create_file(self, mode="new", block=None):
+        return self.File(self, mode, block)
 
-    class inode:
+    def create_directory(self, mode="new", block=None, parent=ROOT_INODE):
+        return self.Directory(self, mode, block, parent)
 
-        def __init__(self):
-            self.size = 0
-            # for now, just direct blocks. might change later
-            self.direct_blocks = []
+    # pad bytes to ensure it is aligned to a certain size
+    def pad(self, bytes, align):
+        padding = b""
+        if len(bytes) % align != 0:
+            padding = b"\0" * (align - (len(bytes) % align))
+        return bytes + padding
+
+    class Inode:
+
+        def __init__(self, filesystem, mode="new", filetype = 0, block=None):
+            self.fs = filesystem
+            if mode == "block" and block is not None: # initialize using preexisting block
+                data = {}
+                ld.readBlock(self.fs.current_disk, block, data)
+                bytes = data['block']
+                self.size = bytes[0]
+                self.filetype = bytes[1] # 0 for regular file, 1 for directory
+                self.num_blocks = bytes[2]
+                self.data_blocks = bytes[3:3 + self.num_blocks] # for now, just direct blocks
+            elif mode == "new": # create new Inode
+                self.size = 0
+                self.filetype = filetype
+                self.num_blocks = 0
+                self.data_blocks = []
+            else: # wrong format
+                raise ValueError(f"unknown mode type {mode}")
 
         def encode(self):
-            return bytearray([self.size] + self.direct_blocks)
+            return bytearray([self.size, self.filetype, self.num_blocks] + self.data_blocks)
+
+        def __str__(self):
+            return f"Inode(Size: {self.size}, Filetype: {self.filetype}, # Blocks: {self.num_blocks}, Blocks: {self.data_blocks})"
+
+
+    class File:
+        def __init__(self, filesystem, mode="new", block=None):
+            self.fs = filesystem
+            if mode == "block" and block is not None:
+                self.inode = self.fs.Inode(self.fs, mode="block", block=block)
+            elif mode == "new":
+                self.inode = self.fs.Inode(self.fs, mode="new", filetype=0)
+        
+        def write(self, bytes, size):
+            # ensure bytes are aligned to blocksize
+            bytes = self.fs.pad(bytes, BLOCKSIZE)
+            num_blocks = math.ceil(size / BLOCKSIZE)
+            if num_blocks > self.inode.num_blocks:
+                new_blocks = self.fs.allocate(num_blocks - self.inode.num_blocks)
+                if new_blocks:
+                    self.inode.data_blocks = self.inode.data_blocks + new_blocks
+                else:
+                    return -4 # no space
+            i = 0
+            while i < num_blocks:
+                start = i * BLOCKSIZE
+                end = (i + 1) * BLOCKSIZE
+                buffer = {}
+                buffer['block'] = bytes[start:end]
+                ld.writeBlock(self.fs.current_disk, self.inode.data_blocks[i], buffer)
+                i += 1
+            # if extra space, free unused blocks
+            self.inode.data_blocks = self.inode.data_blocks[:i]
+            self.fs.free(self.inode.data_blocks[i:])
+            self.inode.size = size
+            self.inode.num_blocks = i
+
+
+    class Directory(File):
+        
+        MAX_FILES_PER_DIRECTORY = BLOCKSIZE // 9 # number of name/inode pairs that can fit within one data block
+
+        def __init__(self, filesystem, mode="new", block=None, parent=ROOT_INODE):
+            if block is None:
+                raise ValueError("must provide Inode block number for directory")
+
+            self.num_files = 0
+            self.files = {}
+            if mode == "block" and block is not None:
+                super().__init__(filesystem=filesystem, mode=mode, block=block)
+                self.files = {}
+                num_files = self.inode.size // 9 # 1 byte for Inode number + 8 bytes for file name = 9 bytes per file
+
+                data = {}
+                ld.readBlock(self.fs.current_disk, self.inode.data_blocks[0], data)
+                bytes = data['block']
+                for i in range(num_files):
+                    start = 9 * i
+                    end = 9 * (i + 1)
+                    pair = bytes[start:end]
+                    # first byte of each 9 byte sequence is the block number of the Inode
+                    inode_block_num = pair[0]
+                    inode_name = []
+                    for j in range(1, 9):
+                        # look for null terminator
+                        if pair[j] == 0:
+                            break
+                        inode_name.append(pair[j].to_bytes(1, byteorder='big'))
+                    inode_name = b"".join(inode_name)
+                    self.add_file(inode_name, inode_block_num)
+                    i += 1
+
+            elif mode == "new":
+                self.fs = filesystem
+                # initialize with pointers to self and parent
+                self.inode = self.fs.Inode(self.fs, "new", filetype=1)
+                self.write(b"\0" * BLOCKSIZE, BLOCKSIZE)
+                self.add_file(self.fs.pad(b".", MAX_FILENAME), block)
+                self.add_file(self.fs.pad(b"..", MAX_FILENAME), parent)
+        
+        def add_file(self, filename, inode):
+            if self.num_files > self.MAX_FILES_PER_DIRECTORY:
+                return -1 # too many files
+            data = {}
+            start = self.num_files * 9
+            end = start + 9
+            ld.readBlock(self.fs.current_disk, self.inode.data_blocks[0], data)
+            bytes = data['block']
+            bytes = bytes[:start] + inode.to_bytes(1, byteorder='big') + filename
+            self.num_files += 1
+            self.write(bytes, self.num_files * 9)
+            self.files[filename] = inode
+        
+        def __str__(self):
+            return f"Directory(# Files: {self.num_files}, Files: {self.files})"
--- libTinyFsDemo.py
+++ libTinyFsDemo.py
@@ -5,5 +5,7 @@ if __name__ == "__main__":
     fs = tinyFS()
     fs.tfs_mkfs()
     fs.tfs_mount(DEFAULT_DISK_NAME)
-    fs.tfs_open("hello.txt")
+    fd = fs.tfs_open("hello.txt")
+    print(f"FD: {fd}")
+    fs.tfs_open("hello.txt") # testing opening the same file
     print(fs)
