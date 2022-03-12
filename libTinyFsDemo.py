1f5bd008bb9023de3c501a7aa9ac19478343ef20
Ash Parasa <ashingtondc@gmail.com>
2022-03-11 16:04:33 -0800

    finished!

--- libTinyFsDemo.py
+++ libTinyFsDemo.py
@@ -2,6 +2,8 @@ from libTinyFS import *
 
 
 if __name__ == "__main__":
+    print("TinyFS Demo")
+    print("\nTesting mkfs, mount, open\n")
     fs = tinyFS()
     fs.tfs_mkfs()
     fs.tfs_mount(DEFAULT_DISK_NAME)
@@ -10,6 +12,7 @@ if __name__ == "__main__":
     print(f"FD: {fd}")
     fs.tfs_open("hello") # testing opening the same file
     print(fs)
+    print("\nTesting writing and reading\n")
     data = {}
     data['bytes'] = "Record a short video (fewer than 5 minutes) demonstrating your file system (using your demo program described above). All members should discuss some of the design decisions you made, limitations of the system, as well as demonstrating the additional features you implemented."
     data['bytes'] = bytearray(data['bytes'], 'utf-8')
@@ -24,6 +27,8 @@ if __name__ == "__main__":
     result = "".join(result)
     print(result)
 
+    print("\nTesting seek\n")
+
     # Test seek
     fs.tfs_seek(fd, 250)
     result = []
@@ -33,8 +38,7 @@ if __name__ == "__main__":
     print(len(result))
     print(result)
 
-    print(fs.free_fds)
-
+    print("\nClosing and reopening the same file: \n")
 
     # Test closing and reopening file
     fs.tfs_close(fd)
@@ -48,11 +52,30 @@ if __name__ == "__main__":
         result.append(chr(buffer['byte']))
     result = "".join(result)
     print(result)
-    # fs.tfs_delete(fd)
 
+    print("\nCreating a different file\n")
 
     fs.tfs_close(fd)
+    # Open new file
+    fd1 = fs.tfs_open("newfile")
+    data = {}
+    data['bytes'] = "This is a different file"
+    data['bytes'] = bytearray(data['bytes'], 'utf-8')
+    fs.tfs_write(fd1, data)
+    # Read the new file as well
+    result = []
+    while(fs.tfs_readByte(fd1, buffer) == 0):
+        result.append(chr(buffer['byte']))
+    result = "".join(result)
+    print(result)
 
+    print("\nOpening hello to delete it\n")
+    fd = fs.tfs_open("hello")
+    print(fs.free_block_table)
+    fs.tfs_delete(fd)
+    print(fs.free_block_table)
+    # Unmount
+    print("\nTesting unmount\n")
     fs.tfs_unmount()
 
 
